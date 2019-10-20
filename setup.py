697ba8608acda9f19f400e4c21698dbd81bf5188
Nicolas M. Thiéry <nthiery@users.sf.net>
2019-10-20 22:02:56 +0200

    setup.py: fix broken installation by using find_packages

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ Semantic annotations for the SageMath library
 import os
 import sys
 # Always prefer setuptools over distutils
-from setuptools import setup
+from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 # To use a consistent encoding
 from codecs import open
@@ -41,7 +41,7 @@ setup(
         #'Programming Language :: Python :: 3',
     ],
     keywords='SageMath',
-    packages=['sage_annotations'],
+    packages=find_packages(),
     install_requires=['recursive-monkey-patch'], # 'Sage'
     cmdclass = {'test': SageTest},
 )
